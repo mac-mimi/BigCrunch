c545840e35d8ca455d2ed2bd301c3c38fdf31601
Spencer Florence <florence@ccs.neu.edu>
2013-09-16 21:45:39 -0400

    stuff

--- playground/gl-test.rkt
+++ playground/gl-test.rkt
@@ -1,5 +1,5 @@
 #lang racket
-(require mred
+(require (prefix-in gui: racket/gui)
          ffi/unsafe
          ffi/unsafe/define
          sgl/gl
@@ -20,61 +20,57 @@
 (define-gl glBufferData (_fun _gl-uint _gl-sizei _gl-floatv _gl-uint -> _void))
 
 (define (gl-gen-buffers size)
-  (gl-vector->vector (glGenBuffers size (make-gl-int-vector size))))
+  (apply values (vector->list (gl-vector->vector (glGenBuffers size (make-gl-int-vector size))))))
 
-(define frame (new frame% [label "fuck"]))
-
+(define frame (new gui:frame% [label "fuck"]))
+  (define test-canvas%
+    (class gui:canvas%
+      
+      (super-instantiate () (style '(gl no-autoclear)) (stretchable-width #f) (stretchable-height #f))
+      
+      (inherit with-gl-context)
+      
+      (define-values (triangle-vertex-buffer triangle-color-buffer)
+        (with-gl-context (thunk (gl-gen-buffers 2))))
+      
+      (define vertexes
+        (vector (- (/ (send this get-width) 2))
+                (- (/ (send this get-height) 2))
+                0
+                5
+                (/ (send this get-width) 2)
+                (- (/ (send this get-height) 2))
+                0
+                5
+                0
+                (/ (send this get-height) 2)
+                0
+                5))
+      (define color (vector 1.0 0.0 0.0 1.0 0.0 1.0 0.0 0.5 0.0 0.0 1.0 0.5))
+      (define scale
+        (vector (/ 1 (send this get-width)) 0 0 0 
+                0 (/ 1 (send this get-height)) 0 0
+                0 0 1 0
+                0 0 0 1))
+      
+      (define/override (on-paint)
+        (with-gl-context draw))
+      
+      (define (init)
+        (glClearColor 0 0 0 .4)
+        (glDepthRange .1 100)
+        (glViewport 0 0 (send this get-width) (send this get-height))
+        (glClear GL_COLOR_BUFFER_BIT)
+        
+        (glBindBuffer GL_ARRAY_BUFFER triangle-vertex-buffer)
+        (glBufferData GL_ARRAY_BUFFER (vector-length vertexes) (vector->gl-float-vector vertexes) GL_STATIC_DRAW)
+        #;(glBindBuffer GL_ARRAY_BUFFER triangle-color-buffer)
+        #;(glBufferData GL_ARRAY_BUFFER (vector-length color) (vector->gl-float-vector color) GL_STATIC_DRAW))
+      
+      (define (draw) #f)
+      (inspect #f)
+      (with-gl-context init)))
 (define canvas 
-  (new 
-   (class canvas%
-     
-     (super-instantiate () (style '(gl no-autoclear)) (stretchable-width #f) (stretchable-height #f))
-     
-     (inherit with-gl-context)
-     
-     (define triangle-vertex-buffer #f)
-     (define triangle-color-buffer #f)
-     (define vertexes
-       (vector (- (/ (send this get-width) 2))
-               (- (/ (send this get-height) 2))
-               0
-               5
-               (/ (send this get-width) 2)
-               (- (/ (send this get-height) 2))
-               0
-               5
-               0
-               (/ (send this get-height) 2)
-               0
-               5))
-     (define color (vector 1.0 0.0 0.0 1.0 0.0 1.0 0.0 0.5 0.0 0.0 1.0 0.5))
-     (define scale
-       (vector (/ 1 (send this get-width)) 0 0 0 
-               0 (/ 1 (send this get-height)) 0 0
-               0 0 1 0
-               0 0 0 1))
-     
-     (define/override (on-paint)
-       (with-gl-context draw))
-     
-     (define (init)
-       (glClearColor 0 0 0 .4)
-       (glDepthRange .1 100)
-       (glViewport 0 0 (send this get-width) (send this get-height))
-       (glClear GL_COLOR_BUFFER_BIT)
-       
-       (define v (gl-gen-buffers 2))
-       (set! triangle-vertex-buffer (vector-ref v 0))
-       (set! triangle-vertex-buffer (vector-ref v 1))
-       
-       (glBindBuffer GL_ARRAY_BUFFER triangle-vertex-buffer)
-       (glBufferData GL_ARRAY_BUFFER (vector-length vertexes) (vector->gl-float-vector vertexes) GL_STATIC_DRAW)
-       (glBindBuffer GL_ARRAY_BUFFER triangle-color-buffer)
-       (glBufferData GL_ARRAY_BUFFER (vector-length color) (vector->gl-float-vector color) GL_STATIC_DRAW))
-     
-     (define (draw) #f)
-     (inspect #f)
-     (with-gl-context init))
-   [parent frame] [min-width 500] [min-height 500]))
+  (new test-canvas% [parent frame] [min-width 500] [min-height 500]))
 
 (send frame show #t)
